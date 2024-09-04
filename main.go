b6308ca979b3c3adf0491688ce466ae7c5f1febd
yossy4411 <yossy4411.dev@gmail.com>
2024-09-04 21:44:35 +0900

    地理院タイルをダウンロードしファイルに保存できるようにした

--- main.go
+++ main.go
@@ -21,6 +21,60 @@ func main() {
 		return
 	}
 	fmt.Println("指定されたタイルのIDは「" + tileID + "」です。")
+
+	fmt.Println("タイルのズームレベルの範囲を指定してください。")
+	fmt.Print("最小ズームレベル [5]: ")
+	var minZoomLevel, maxZoomLevel int
+
+	_, err = fmt.Scan(&minZoomLevel)
+	if err != nil {
+		fmt.Println("入力エラーが発生しました。")
+		return
+	}
+
+	fmt.Print("最大ズームレベル [16]: ")
+	_, err = fmt.Scan(&maxZoomLevel)
+	if err != nil {
+		fmt.Println("入力エラーが発生しました。")
+		return
+	}
+
+	fmt.Println("指定されたズームレベルの範囲は" + fmt.Sprint(minZoomLevel) + "から" + fmt.Sprint(maxZoomLevel) + "です。")
+
 	client := downloader.NewDownloader(tileID, 10)
+	err = client.SetTargetDir("tiles")
+	if err != nil {
+		fmt.Println("ターゲットディレクトリの作成に失敗しました。")
+		fmt.Println(err)
+		return
+	}
+	client.ZoomMin = minZoomLevel
+	client.ZoomMax = maxZoomLevel
 	fmt.Println("mokuroku.csvファイルを取得し、タイルのURLを取得します。")
+	mokuroku := client.GetMokurokuURL()
+	fmt.Println("mokuroku.csv: " + mokuroku)
+	err = client.GetURLs(mokuroku)
+	if err != nil {
+		fmt.Println("mokuroku.csvファイルの取得に失敗しました。")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("指定されたズームレベルの範囲で見つかったタイルは" + fmt.Sprint(client.GetTileCount()) + "個です。")
+	fmt.Println("また、タイルの範囲は" + fmt.Sprint(client.ZoomMin) + "から" + fmt.Sprint(client.ZoomMax) + "です。")
+	fmt.Println("タイルのダウンロードを開始します。")
+
+	for {
+		err = client.DownloadTile()
+		if err != nil {
+			fmt.Println("タイルのダウンロードに失敗しました。")
+			fmt.Println(err)
+			return
+		}
+		if client.IsFinished() {
+			break
+		}
+		// プログレスバーを表示します。
+		fmt.Printf("\rダウンロード中... %d/%d", client.Downloaded, client.GetTileCount())
+	}
+
 }
